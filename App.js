97a9c44a59febb1253431c8df6ab1e30e6cc487d
cliffordru <clifford.gray@gmail.com>
2016-06-05 14:33:17 -0400

    Class component vs stateless function component

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+
+// Class Component example using JSX (HTML Like Syntax) transpiled to JS
+// Class component can have state
+// Ctrl / Ctrl Shift /
+
 class App extends React.Component {
 	render(){
-		return <div>Hi</div>
+		return <div>Hello World</div>
 	}
 }
+
+// Stateless function component
+// const App = () => <h1>Hello Dude</h1>
+
 
 export default App
