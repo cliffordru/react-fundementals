2bd16f3bc11b3c193a82753fab817dad10d64b45
cliffordru <clifford.gray@gmail.com>
2016-06-05 14:51:03 -0400

    Introduction to Properties - defaultProps, propTypes

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Class Component example using JSX (HTML Like Syntax) transpiled to JS
 // Class component can have state
@@ -10,17 +11,31 @@ class App extends React.Component {
 		// this is because JSX is a function React.createElement
 		// cannot return two functions.  
 		//To resolve wrap into single node
+		let txt = this.props.txt
 		return (
 			<div>
-				<h1>Hello World</h1>
-				<b>bold</b>
+				<h1>{txt}</h1>
 			</div>
 			);
 	}
 }
 
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'this is the default txt'
+}
+
+ReactDOM.render(
+	<App cat={5} />,
+	document.getElementById('app')
+);
+
 // Stateless function component
 // const App = () => <h1>Hello Dude</h1>
 
 
-export default App
+//export default App
