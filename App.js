9dba3a9eef01e4c852381b4a7f540412d35ab217
cliffordru <clifford.gray@gmail.com>
2016-06-12 17:11:08 -0400

    Example of using refs

--- App.js
+++ App.js
@@ -7,6 +7,69 @@ class App extends React.Component{
 	constructor(){
 		super(); // give context to this
 		this.state = {
+			red: 0,
+			green: 0,
+			blue: 0
+		} // set state to an object
+		this.update = this.update.bind(this) // So we can change onChange with shorthand
+	}
+	update(e){ // manage state; take in an event
+		this.setState({
+			red:ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+			green:ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+			blue:ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+		})
+		// do not have to pass entire state, will merge with cat
+	}
+	render(){
+		return (
+			<div>
+				
+				<hr />
+				<Slider ref="red" update={this.update} />
+				{this.state.red}
+				<br />
+				<Slider ref="green" update={this.update} />
+				{this.state.green}
+				<br />
+				<Slider ref="blue" update={this.update} />
+				{this.state.blue}
+				<br />
+			</div>
+			)
+	}
+}
+
+class Slider extends React.Component{
+	render(){
+		return (
+			<div>
+				<input ref="inp" type="range"
+					min="0" 
+					max="255"
+					onChange={this.props.update} />
+			</div>
+			)
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
+
+//export default App
+
+/*  PREVIOUS STUFF...
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Owner Ownee relationship 
+class App extends React.Component{
+	constructor(){
+		super(); // give context to this
+		this.state = {
 			txt: 'this is the state txt',
 			cat: 0
 		} // set state to an object
@@ -30,10 +93,10 @@ class App extends React.Component{
 // adding a stateless component
 const Widget = (props) => {
 	return (
-			<div>
+			<div>Type in me... <br />
 			<input type="text" 
 				onChange={props.update} />
-			<h1>{props.txt}</h1>
+			<h2>{props.txt}</h2>
 			</div>
 			)
 }
@@ -44,10 +107,9 @@ ReactDOM.render(
 );
 
 //export default App
+*/
 
-
-
-/*
+/*  PREVIOUS STUFF...
 
 import React from 'react';
 import ReactDOM from 'react-dom';
