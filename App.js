37d264128cf30dad41542704936ba6bb3b6aad2b
cliffordru <clifford.gray@gmail.com>
2016-06-19 10:32:18 -0400

    Component lifecycle - mounting basics

--- App.js
+++ App.js
@@ -2,6 +2,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Component lifecycle - mounting basics
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state = {val: 0};
+		this.update = this.update.bind(this);
+	}
+	update(){
+		this.setState({val: this.state.val +1})
+	}
+	componentWillMount() {
+		console.log('mounting')
+	}
+	render(){
+		console.log('rendering!')
+		return <button onClick={this.update}>{this.state.val}</button>
+	}
+	componentDidMount() {
+		console.log('mounted!')
+	}
+	componentWillUnmount() {
+		console.log('bye!')
+		// requires a wrapper - see https://egghead.io/lessons/react-component-lifecycle-mounting-basics @2:45
+	}
+}
+
+class Wrapper extends React.Component {
+  constructor(){
+  	super();
+  }
+  mount(){
+  	ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+  	ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render(){
+    return (
+    	<div>
+    		<button onClick={this.mount.bind(this)}>Mount</button>
+    		<button onClick={this.unmount.bind(this)}>Unmount</button>
+    		<div id="a"></div>
+    	</div>
+    )
+  }
+}
+
+ReactDOM.render(
+	<Wrapper />,
+	document.getElementById('app')
+);
+
+//export default App
+// export default Wrapper
+
+/*  PREVIOUS STUFF...
+
 // Access child properties - angular transclusion
 class App extends React.Component{
 	render(){
@@ -25,9 +82,7 @@ ReactDOM.render(
 	document.getElementById('app')
 );
 
-//export default App
 
-/*  PREVIOUS STUFF...
 
 //Owner Ownee relationship 
 class App extends React.Component{
